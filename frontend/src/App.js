4a4dcd0a9d7c4e7a90dba57b3fb7e2fec64cf557
Arnav <arnavbiswas2000@gmail.com>
2020-10-05 00:36:12 +0530

    react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hey user, Welcome to our perfume shop !!</h1>
+    </div>
+  );
+};
+
+export default App;
