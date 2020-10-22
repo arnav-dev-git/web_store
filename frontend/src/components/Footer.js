18cfd12cad8d48743e1434d09abce0d8f4dbf15c
Arnav <arnavbiswas2000@gmail.com>
2020-10-22 23:42:19 +0530

    footer -> year added !

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
+  const date = new Date().getFullYear();
+
   return (
     <footer>
       <Container>
         <Row>
           <Col className="text-center mx-0 px-0">
-            <p>Copyright © ElectronStore</p>
+            <p>Copyright © ElectronStore {date}</p>
           </Col>
         </Row>
       </Container>
