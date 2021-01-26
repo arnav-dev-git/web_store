4d1f609f8e91a5ad1a6e40a609956fb744c1a3f4
Arnav <arnavbiswas2000@gmail.com>
2021-01-27 02:23:37 +0530

    get user profile is set

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -6,7 +6,7 @@ import User from "../models/userModel.js";
 //! @route GET /api/users/login
 //& @access public
 
-export const authUser = asyncHandler(async (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
@@ -29,6 +29,20 @@ export const authUser = asyncHandler(async (req, res) => {
 //! @route GET /api/users/profile
 //& @access private
 
-export const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("success");
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
+
+export { authUser, getUserProfile };
