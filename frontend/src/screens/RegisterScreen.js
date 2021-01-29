b2aa04481912b8c4bcb9ace75727913a895b3374
Arnav <arnavbiswas2000@gmail.com>
2021-01-30 02:19:00 +0530

    registration process is done | frontend

---
+++ frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import FormContainer from "../components/FormContainer";
+import { register } from "../actions/userActions";
+
+const RegisterScreen = ({ location, history }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const userRegister = useSelector((state) => state.userRegister);
+  const { loading, error, userInfo } = userRegister;
+
+  const subminHandler = (e) => {
+    e.preventDefault();
+    // DISPATCH REGISTER
+    if (password !== confirmPassword) {
+      setMessage("Password do not match !");
+    } else {
+      setMessage(null);
+      dispatch(register(name, email, password));
+    }
+  };
+
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+
+  const userInfoInLocalStorage = localStorage.getItem("userInfo");
+
+  useEffect(() => {
+    if (userInfoInLocalStorage && userInfo) {
+      history.push(redirect);
+    }
+  }, [history, userInfoInLocalStorage, userInfo, redirect]);
+
+  return (
+    <FormContainer>
+      <h1>Sign Up</h1>
+      {error && <Message variant="success">{error}</Message>}
+      {message && <Message variant="success">{message}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={subminHandler}>
+        <Form.Group controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your Name"
+            value={name}
+            required={true}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            required={true}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            required={true}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="confirmPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm Passwword"
+            value={confirmPassword}
+            required={true}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type="submit" variant="info">
+          Sign UP
+        </Button>
+      </Form>
+
+      <Row className="py-3">
+        <Col>
+          Have an account ?{" "}
+          <Link
+            to={redirect ? `/login?redirect=${redirect}` : "/login"}
+            className="mx-1"
+          >
+            {" "}
+            Log In
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default RegisterScreen;
