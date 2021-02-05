b0b608ee4ca1e460f5c581915024948a8f2517e3
Arnav <arnavbiswas2000@gmail.com>
2021-02-06 00:10:05 +0530

    user profile filled name and mail | frontend

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import Message from "../components/Message";
@@ -24,8 +24,15 @@ const ProfileScreen = ({ location, history }) => {
   useEffect(() => {
     if (!userInfo) {
       history.push("/login");
+    } else {
+      if (!user.name) {
+        dispatch(getUserDetails("profile"));
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+      }
     }
-  }, [history, userInfo]);
+  }, [dispatch, history, userInfo, user]);
 
   const subminHandler = (e) => {
     e.preventDefault();
@@ -33,83 +40,79 @@ const ProfileScreen = ({ location, history }) => {
     if (password !== confirmPassword) {
       setMessage("Password do not match !");
     } else {
-      setMessage(null);
-      //   dispatch(register(name, email, password));
+      //! DISPATCH UPDATE PROFILE
     }
   };
 
-  const redirect = location.search ? location.search.split("=")[1] : "/";
+  // const redirect = location.search ? location.search.split("=")[1] : "/";
 
-  const userInfoInLocalStorage = localStorage.getItem("userInfo");
+  // const userInfoInLocalStorage = localStorage.getItem("userInfo");
 
-  useEffect(() => {
-    if (userInfoInLocalStorage && userInfo) {
-      history.push(redirect);
-    }
-  }, [history, userInfoInLocalStorage, userInfo, redirect]);
+  // useEffect(() => {
+  //   if (userInfoInLocalStorage && userInfo) {
+  //     history.push(redirect);
+  //   }
+  // }, [history, userInfoInLocalStorage, userInfo, redirect]);
 
   return (
     <>
-      <h1>Sign Up</h1>
-      {error && <Message variant="success">{error}</Message>}
-      {message && <Message variant="success">{message}</Message>}
-      {loading && <Loader />}
-      <Form onSubmit={subminHandler}>
-        <Form.Group controlId="name">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter your Name"
-            value={name}
-            required={true}
-            onChange={(e) => setName(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="email">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            required={true}
-            onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter your password"
-            value={password}
-            required={true}
-            onChange={(e) => setPassword(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group controlId="confirmPassword">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Confirm Passwword"
-            value={confirmPassword}
-            required={true}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
-        <Button type="submit" variant="info">
-          Sign UP
-        </Button>
-      </Form>
-
-      <Row className="py-3">
-        <Col>
-          Have an account ?{" "}
-          <Link
-            to={redirect ? `/login?redirect=${redirect}` : "/login"}
-            className="mx-1"
-          >
-            {" "}
-            Log In
-          </Link>
+      <Row>
+        <Col md={3}>
+          <h2>User Profile</h2>
+          {error && <Message variant="success">{error}</Message>}
+          {message && <Message variant="success">{message}</Message>}
+          {loading ? (
+            <Loader />
+          ) : (
+            <Form onSubmit={subminHandler}>
+              <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter your Name"
+                  value={name}
+                  required={true}
+                  onChange={(e) => setName(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="email">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter your email"
+                  value={email}
+                  required={true}
+                  onChange={(e) => setEmail(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  required={true}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="confirmPassword">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Confirm Passwword"
+                  value={confirmPassword}
+                  required={true}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Button type="submit" variant="info">
+                Update
+              </Button>
+            </Form>
+          )}
+        </Col>
+        <Col md={9}>
+          <h2>Your Orders</h2>
         </Col>
       </Row>
     </>
