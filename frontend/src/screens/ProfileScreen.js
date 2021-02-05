a84968fa1f870313e294bc94d6add7c7a36998da
Arnav <arnavbiswas2000@gmail.com>
2021-02-06 01:27:48 +0530

    navbar name update bug fixed after profile edit | frontend

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -27,14 +27,15 @@ const ProfileScreen = ({ location, history }) => {
     if (!userInfo) {
       history.push("/login");
     } else {
-      if (!user.name) {
+      if (!user || !user.name || success) {
+        dispatch({ type: "USER_UPDATE_PROFILE_RESET" });
         dispatch(getUserDetails("profile"));
       } else {
         setName(user.name);
         setEmail(user.email);
       }
     }
-  }, [dispatch, history, userInfo, user]);
+  }, [dispatch, history, userInfo, user, success]);
 
   const submitHandler = (e) => {
     e.preventDefault();
